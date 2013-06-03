8ba275689330f40aa9e00743a1a1719aee1a3839
mansisolanki <indianic@ind509.local>
2013-06-03 18:02:12 +0530

    ghfgh

--- DemoAlertView/DemoAlertView/ViewController.m
+++ DemoAlertView/DemoAlertView/ViewController.m
@@ -28,7 +28,7 @@
 
 -(IBAction)btnClickMe:(id)sender
 {
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Alert Demo" message:@"Type Message Here!!!" delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Alert Demo" message:@"Hello!!!" delegate:self cancelButtonTitle:@"OK" otherButtonTitles: nil];
     [alert show];
     [alert release];
 }
